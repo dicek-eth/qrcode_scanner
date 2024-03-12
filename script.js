dfbd8bc8d1d5fbdb354864915830734c9da1e823
dicek.eth <daidaisuke@yahoo.co.jp>
2024-03-12 15:13:55 +0900

    updated design

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const video = document.createElement('video');
-document.body.appendChild(video); // この行を追加
-const canvasElement = document.getElementById('qr-canvas');
-const canvas = canvasElement.getContext('2d', { willReadFrequently: true });
-const loading = document.getElementById('loading');
-const output = document.getElementById('output');
-let isReadQR = false;
- 
-navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+document.addEventListener("DOMContentLoaded", function() {
+  const video = document.getElementById('qr-video');
+  const canvasElement = document.getElementById('qr-canvas');
+  const canvas = canvasElement.getContext('2d', { willReadFrequently: true });
+  const loading = document.getElementById('loading');
+  let isReadQR = false;
+
+  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
   .then((stream) => {
-    video.srcObject = stream;
-    video.setAttribute('playsinline', true);
-    video.play();
-    requestAnimationFrame(tick);
+      video.srcObject = stream;
+      video.setAttribute('playsinline', true);
+      video.play();
+      requestAnimationFrame(tick);
   });
- 
-function tick() {
-  loading.innerText = 'ロード中...';
-  if (video.readyState === video.HAVE_ENOUGH_DATA) {
-    loading.hidden = true;
-    canvasElement.hidden = false;
-    canvasElement.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-    var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-    var code = jsQR(imageData.data, imageData.width, imageData.height, {
-      inversionAttempts: 'dontInvert',
-    });
-    if (code && !isReadQR) {
-      alert(`スキャン結果: ${code.data}`); // 結果をアラートで表示
-      isReadQR = true; // 一度スキャンしたら再スキャンしない
-    }
+
+  function tick() {
+      if (video.readyState === video.HAVE_ENOUGH_DATA) {
+          loading.style.display = 'none';
+          canvasElement.height = video.videoHeight;
+          canvasElement.width = video.videoWidth;
+          canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+          const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+          const code = jsQR(imageData.data, imageData.width, imageData.height, {
+              inversionAttempts: 'dontInvert',
+          });
+          if (code && !isReadQR) {
+              const result = document.getElementById('result');
+              result.textContent = `スキャン結果: ${code.data}`;
+              isReadQR = true;
+          }
+      }
+      if (!isReadQR) requestAnimationFrame(tick);
   }
-  if (!isReadQR) requestAnimationFrame(tick); // QRコードをまだ読み取っていなければ、繰り返す
-}
+});
