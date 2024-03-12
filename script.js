9918e840dcbf3b8b32e119b9567a606451273c4f
dicek.eth <daidaisuke@yahoo.co.jp>
2024-03-12 10:33:31 +0900

    updated index.html

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const videoElement = document.getElementById('qr-video');
-  const canvasElement = document.getElementById('qr-canvas');
-  const canvasContext = canvasElement.getContext('2d');
+const video = document.createElement('video');
+const canvasElement = document.getElementById('canvas');
+const canvas = canvasElement.getContext('2d');
+const loading = document.getElementById('loading');
+let isReadQR = false;
+// ES6 import
+import jsQR from "jsqr";
+ 
+// CommonJS require
+const jsQR = require("jsqr");
 
-  // カメラを起動
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-      .then(function(stream) {
-          videoElement.srcObject = stream;
-          videoElement.setAttribute("playsinline", true); // iOS用の設定
-          videoElement.play();
-          requestAnimationFrame(tick);
-      });
+navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+  .then((stream) => {
+    video.srcObject = stream;
+    video.setAttribute('playsinline', true);
+    video.play();
+    requestAnimationFrame(tick);
+  });
 
-  function tick() {
-      if (videoElement.readyState === videoElement.HAVE_ENOUGH_DATA) {
-          // Videoの表示をCanvasに転送
-          canvasElement.height = videoElement.videoHeight;
-          canvasElement.width = videoElement.videoWidth;
-          canvasContext.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
-          var imageData = canvasContext.getImageData(0, 0, canvasElement.width, canvasElement.height);
-          // QRコードをデコード
-          var code = jsQR(imageData.data, imageData.width, imageData.height, {
-              inversionAttempts: "dontInvert",
-          });
-          if (code) {
-              document.getElementById('result').textContent = `スキャン結果: ${code.data}`;
-              // QRコードのスキャンを停止
-              //videoElement.pause();
-              //videoElement.srcObject.getTracks().forEach(track => track.stop());
-          } else {
-              requestAnimationFrame(tick);
-          }
-      } else {
-          requestAnimationFrame(tick);
-      }
+function tick() {
+  loading.textContent = '⌛ ロード中...';
+  if (video.readyState === video.HAVE_ENOUGH_DATA) {
+    loading.hidden = true;
+    canvasElement.hidden = false;
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+    var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+    var code = jsQR(imageData.data, imageData.width, imageData.height, {
+      inversionAttempts: 'dontInvert',
+    });
+    if (code && !isReadQR) {
+      location.href = code.data;
+      isReadQR = true;
+    }
   }
-});
+  requestAnimationFrame(tick);
+}
